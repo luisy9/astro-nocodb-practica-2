4e8305b650480fd0e7ce7903fb4b95de1cbbfa86
Luis de Haro <luisdeharo4@gmail.com>
2024-02-02 11:44:21 +0100

    update food

--- src/classes/ReceptesController.js
+++ src/classes/ReceptesController.js
@@ -2,91 +2,94 @@ const APIURL = 'https://app.nocodb.com/api/v2/tables/mf43qftkiil3yxz/records';
 const TOKEN = 'oM9bCiNOHfyovqNhKC6kKdBXCxExmMcpVbi-XItZ';
 
 class ReceptesController {
-    constructor() {
-        this.apiUrl = APIURL;
-        this.token = TOKEN;
-    }
+  constructor() {
+    this.apiUrl = APIURL;
+    this.token = TOKEN;
+  }
 
-    async getAllReceptes() {
-        const response = await fetch(`${this.apiUrl}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'xc-token': this.token
-            }
-        });
+  async getAllReceptes() {
+    const response = await fetch(`${this.apiUrl}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'xc-token': this.token,
+      },
+    });
 
-        const data = await response.json();
-        return data;
-    }
+    const data = await response.json();
+    return data;
+  }
 
-    async getReceptaById(id) {
-        const response = await fetch(`${this.apiUrl}/${id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'xc-token': this.token
-            }
-        });
+  async getReceptaById(id) {
+    const response = await fetch(`${this.apiUrl}/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'xc-token': this.token,
+      },
+    });
 
-        return response.json();
-    }
+    return response.json();
+  }
 
-    async createRecepta(nom, foto, descripcio) {
-        const response = await fetch(`${this.apiUrl}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'xc-token': this.token
-            },
-            body: JSON.stringify({
-                nom,
-                foto,
-                descripcio
-            })
-        });
+  async createRecepta(nom, foto, descripcio) {
+    const response = await fetch(`${this.apiUrl}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'xc-token': this.token,
+      },
+      body: JSON.stringify({
+        nom,
+        foto,
+        descripcio,
+      }),
+    });
 
-        const data = await response.json();
-        return data;
-    }
+    const data = await response.json();
+    return data;
+  }
 
-    async updateRecepta(id, nom, foto, descripcio) {
-        const response = await fetch(`${this.apiUrl}/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'xc-token': this.token
-            },
-            body: JSON.stringify({
-                nom,
-                foto,
-                descripcio
-            })
-        });
+  async updateRecepta(id, nom, foto, descripcio) {
+    console.log(id, nom, descripcio, foto);
+    const response = await fetch(`${this.apiUrl}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'xc-token': this.token,
+      },
+      body: JSON.stringify({
+        id,
+        nom,
+        foto,
+        descripcio,
+      }),
+    });
 
-        const data = await response.json();
-        return data;
-    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }
 
-    async deleteRecepta(id) {
-        const response = await fetch(`${this.apiUrl}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'xc-token': this.token
-            },
-            body: JSON.stringify({
-                Id:id
-            })
-        });
+  async deleteRecepta(id) {
+    const response = await fetch(`${this.apiUrl}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'xc-token': this.token,
+      },
+      body: JSON.stringify({
+        Id: id,
+      }),
+    });
 
-        const data = await response.json();
-        return data;
-    }
+    const data = await response.json();
+    return data;
+  }
 
-    async updateReceta(id) {
-        console.log(id);
-    }
+  async updateReceta(id) {
+    console.log(id);
+  }
 }
 
 export default ReceptesController;
