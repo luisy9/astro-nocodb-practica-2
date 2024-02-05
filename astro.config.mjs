8a376b8028d786f34b9943291a7ab32034841ab8
Luis de Haro <luisdeharo4@gmail.com>
2024-02-05 12:21:45 +0100

    Dashboard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import astro18next from '@astro-i18next';
+import tailwind from '@astrojs/tailwind';
+import astroI18next from 'astro-i18next';
 
-import netlify from "@astrojs/netlify";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astro18next()],
-  output: "server",
-  adapter: netlify()
+  integrations: [tailwind(), astroI18next()],
+  output: 'server',
+  adapter: netlify(),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': 'inset 1px -5px 40px 20px rgb(220, 20, 0)',
+      },
+    },
+  },
+  plugins: [],
+};
